aaa3f0940cd7dd59915e04c62a8002785a373a9e
pkamanboina <pkamanboina@gmail.com>
2021-02-13 15:58:45 -0800

    mods

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Body from "./Body"
 function App(){
     return (
       <div className="App">
-        <h1>Spotify Stats App</h1>
+        <h1 className="header">Spotify Stats App</h1>
         <main>
         <Body />
         
--- src/Body.js
+++ src/Body.js
@@ -1,72 +1,55 @@
 import React from "react"
-import Spotify from "spotify-web-api-js";
+import queryString from "query-string";
 
-const spotify = new Spotify();
 let params
-let token
+
+
 class Body extends React.Component{
 
   constructor() {
     super()
-    params = this.getHashParams();
-    token = params.access_token;
-    console.log(token)
     this.state = {
-        isLoggedIn: params.access_token ? true : false,
-        topTracks: []
+      serverData: {
+        user: {
+          name: '',
+        }
+      }
     }
-
-    if(params.access_token) {
-        spotify.setAccessToken(params.access_token)
-    }
-}
-
-   /* returns info such as access token after user logs in. If login fails,
-    access token does not exist.
-   */
-  getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
   }
 
-
-    getTopTracks() {
-        spotify.getMyTopTracks(token).then((ans) => {
-            this.setState({
-                topTracks: ans.item
-            })
-            });
-    }
-  
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token
+    fetch("https://api.spotify.com/v1/me", {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({serverData: {user: {name: data.display_name}}}))
+  }
 
   
   render() {
     
-    // FIXME: removes button if user is logged in -- still needs work
-    const styles = {
-        display: this.state.isLoggedIn ? "none" : "blocked"
-    }
+    
     return (
       <div>
-        <button onClick={handleClick} style={styles}> Login </button>
+        <button onClick={handleClick}> Login </button>
+        <br />
         <hr />
-        <br/>
-        <button onClick={() => this.getTopTracks()}>Top Tracks</button>
-                <p>
-                    {this.state.topTracks}
-                </p>
+        {this.state.serverData.user.name ?
+        <h1>
+          Username: {this.state.serverData.user.name}
+        </h1>
+        : [] 
+        }
+
       </div>
+
     );
   }
 }
 
 function handleClick() {
-  window.open("http://localhost:8888");
+  window.open("http://localhost:8888/login");
 }
 
 
