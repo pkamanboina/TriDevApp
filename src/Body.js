6f9f164419def1e45cf6f36bd3c92f4171577700
pkamanboina <pkamanboina@gmail.com>
2021-02-13 22:13:56 -0800

    playlist

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,7 @@
 import React from "react"
 import queryString from "query-string";
-
-let params
+import Playlist from "./Playlist"
+import "./Body.css"
 
 
 class Body extends React.Component{
@@ -9,10 +9,8 @@ class Body extends React.Component{
   constructor() {
     super()
     this.state = {
-      serverData: {
-        user: {
-          name: '',
-        }
+      user: {
+        name: ""
       }
     }
   }
@@ -20,33 +18,81 @@ class Body extends React.Component{
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token
+    if (!accessToken)
+      return;
     fetch("https://api.spotify.com/v1/me", {
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then(response => response.json())
-    .then(data => this.setState({serverData: {user: {name: data.display_name}}}))
+    .then(data => this.setState({user: {name: data.display_name}}))
+
+    
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      playlists: data.items.map(item => {
+        console.log(data.items)
+        return {
+          name: item.name,
+          imageUrl: item.images[0].url, 
+          songs: []
+        }
+    })
+    }))
+    
+    
+
   }
 
   
   render() {
-    
-    
+
+    console.log(this.state.playlists)
     return (
       <div>
         <button onClick={handleClick}> Login </button>
         <br />
         <hr />
-        {this.state.serverData.user.name ?
+        {this.state.user.name ?
         <h1>
-          Username: {this.state.serverData.user.name}
+          Username: {this.state.user.name}
         </h1>
         : [] 
         }
+        <div style={{display: 'flex'}}>
+          {this.state.playlists ?
+          <div className="playlists">
+
+          <h3>Playlists:</h3>
+          <br />
+          {this.state.playlists.map(playlist => 
+              <Playlist playlist={playlist} />
+          )}
+          </div>
+          : []
+          }
+          <div>
+            <h3>Favorite Artists</h3>
+          </div>
+        </div>
+        
 
       </div>
 
     );
   }
 }
+
+/*
+        <div> 
+        
+        {this.state.playlists.map(playlist => 
+            <Playlist playlist={playlist} />
+        )}
+        
+        </div>
+        */
+        
 
 function handleClick() {
   window.open("http://localhost:8888/login");
