f138130b697599913b3bd7f57e017602259a05e1
Kian <kezaz@ucsd.edu>
2021-02-14 03:10:46 -0800

    Added top tracks and corresponding artists

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,7 @@ import React from "react"
 import queryString from "query-string";
 import Playlist from "./Playlist"
 import TopArtists from "./TopArtists"
+import TopTracks from "./TopTracks"
 import "./Body.css"
 
 
@@ -47,13 +48,28 @@ class Body extends React.Component{
   }).then(response => response.json()) 
     .then(data => this.setState({
       topArtists: data.items.map(artist => {
-        console.log(data)
         return {
           name: artist.name,
           imageUrl: artist.images[2].url
         }
       })
     })) 
+
+    fetch('https://api.spotify.com/v1/me/top/tracks', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+  }).then(response => response.json()) 
+    .then(data => this.setState({
+      topTracks: data.items.map(track => {
+        return {
+          name: track.name,
+          artist: track.artists.map(artist => {
+            return (
+              artist.name + " "
+            )
+          })
+        }
+      })
+    }))
 
   }
 
@@ -91,8 +107,23 @@ class Body extends React.Component{
           <div className = "topArtists">
             
             <h3>Top Artists:</h3>
+            <br />
             {this.state.topArtists.map(artist => 
               <TopArtists topArtists = {artist} />
+              )}
+          </div>
+          : []
+          }
+        </div>
+
+        <div>
+          {this.state.topTracks ?
+          <div className = "topTracks">
+
+            <h3>Top Tracks:</h3>
+            <br />
+            {this.state.topTracks.map(track => 
+              <TopTracks topTracks = {track} />
               )}
           </div>
           : []
